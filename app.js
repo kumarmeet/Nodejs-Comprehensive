43394bba789715a74faeb8fb94699dd9927e6402
kumarmeet <kumarmeet51@yahoo.com>
2022-04-21 22:31:36 +0530

    create a nodejs raw server with using core nodejs concept only and parse data into the fileusing buffers and event like on)

--- app.js
+++ app.js
@@ -1,7 +1,49 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    const url = req.url;
+    const method = req.method;
+
+    if (url === "/") {
+        res.write("<html>")
+        res.write("<head><title>Enter Message</title></head>")
+        res.write(`
+        <body>
+            <form action='/message' method='POST'>
+                <input name="text" type='text'/>
+                <button type='submit'>submit</button>
+            </form>
+        </body>
+        `)
+        res.write("</html>")
+        return res.end();
+    }
+
+    if (url === "/message" && method === "POST") {
+        const body = [];
+        
+        req.on("data", (chunk) => {
+            body.push(chunk);
+        });
+
+        req.on("end", () => {
+            const parsedBuffer = Buffer.concat(body).toString();
+            const message = parsedBuffer.split("=")[1].split("+").join(" ");
+            fs.writeFileSync("message.txt", message);
+        });
+        
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+    }
+
+    res.setHeader("Content-Type", "text/html")
+    res.write("<html>")
+    res.write("<head><title>My first page</title></head>")
+    res.write("<body><h1>Hello from my nodejs server.</h1></body>")
+    res.write("</html>")
+    res.end();
 });
 
 server.listen(3000)
